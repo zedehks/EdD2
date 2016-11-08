bef9e159a2046e1811e982be9205f954149baf63
zedehks <waldo9000@gmail.com>
2016-11-07 22:16:44 -0600

    it sorta works lel

--- Hausaufgabe/homework.c
+++ Hausaufgabe/homework.c
@@ -13,10 +13,11 @@ struct person
 };
 
 person* gen_person();
-void printp(person* p);
-void printh();
+void print_p(person* p);
+void print_h();
 
-char* fixed_size(person* p);
+int list_size(person* p);
+char* print_fs(person* p);
 char* length_delim(person* p);
 char* char_delim(person* p);
 char* dir_list(person* p);
@@ -29,40 +30,97 @@ int main (int argc, char *argv[])
 	if(argc < 2)
 	{
 		printf("\nERROR: Insufficient parameters(<1)\n");
-		printh();
+		print_h();
+		return -1;
 	}
 	else if(argc > 2)
 	{
 
 		printf("\nERROR: Excess parameters (>1)\n");
-		printh();
+		print_h();
+		return -1;
 	}
 	else
 	{
-
+		//there's a correct number of arguments, let's go!
 		if(!strcmp("-h",argv[1]))
-			printh();
+			print_h();
 		else 
 		{
-
+			//gen a list of people 
 			person* hito = gen_person();
-			if(!strcmp("-f",argv[1]))
+			hito->next = gen_person();
+			hito->next->next = gen_person();
+			hito->next->next->next = gen_person();
+
+			if(!strcmp("-l",argv[1]))
 			{
-				char* buffer = fixed_size(hito);
 			}
-			printp(hito);
+			else if(!strcmp("-f",argv[1]))
+			{
+				print_p(hito);
+				char* buffer = print_fs(hito);
+				printf("\nString containing list: %s\n",buffer);
+
+			}
+			else
+			{
+				printf("\nERROR: Unknown parameter.\n");
+				print_h();
+				return -1;
+			}
+
+			while(hito!=NULL)
+			{
+				person *tmp = hito;
+				hito = hito->next;
+				free(tmp);
+			}	
 			free(hito);
 		}
 	
 	}
 }
-char* fixed_size(person* p)
+char* print_fs(person* p)
 {
+	char name[20]; 
+	char surname[20]; 
+	int age = 0;
+	char age_s[6];
+	int ls = 44* list_size(p);
+	char buffer[ls];
 
+	int i=0;
+	while(i<ls)	
+	{
+		buffer[i] = 0;
+		i++;
+	}
+	while(p!=NULL)
+	{
+		memcpy(&name,p->given_name,20);
+		memcpy(&surname,p->surname,20);
+		age = p->age;
+		sprintf(age_s,"%d",age);
+		strcat(buffer,name);
+		strcat(buffer,surname);
+		strcat(buffer,age_s);
+		p = p->next;
+	}
+	return buffer;
 }
 
 
-
+int list_size(person* p)
+{
+	int counter = 1;
+	while(p != NULL)
+	{
+		counter++;
+		p = p->next;
+	}
+	return counter;
+}
 
 
 
@@ -77,12 +135,17 @@ person* gen_person()
 	return p;
 }
 //print the persons crap
-void printp(person* p)
+void print_p(person* p)
 {
-	printf("\nName: %s %s\nAge: %d\n\n",p->given_name,p->surname,p->age);
+	while( p!=NULL)	
+	{
+		printf("Name: %s %s\nAge: %d\n",p->given_name,p->surname,p->age);
+		p = p->next;
+	}
+	printf("##END OF LIST##\n");
 }
 //print the helpening help of help halp pls
-void printh()
+void print_h()
 {
 	
 	printf("\nUsage: clone [OPTION]\n");
